d9e2ef143a156638cc736ac400d04a7f53cd9a6b
goDrag0n <39076720+goDrag0n@users.noreply.github.com>
2018-05-08 12:52:22 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,23 +2,23 @@
 
 namespace mgspace{
 /**
-*³¬Éù²¨Ä£¿éSR04²â¾à
+*Â³Â¬Ã‰Ã¹Â²Â¨Ã„Â£Â¿Ã©SR04Â²Ã¢Â¾Ã 
 */
 //% blockId="SR04" block="SR04 Trig %Trig|Echo %Echo"
 //% blockGap=2 weight=0 blockExternalInputs=true
-export function SR04(Trig:DigitalPin,Echo:DigitalPin):number{
-pins.digitalWritePin(Trig,1);
-control.waitMicros(20);
-pins.digitalWritePin(Trig,0);
-const tim=input.runningTimeMicros();
-while(pins.digitalReadPin(Echo)==0 && input.runningTimeMicros()-tim<20000)
-{
-}
-tim=input.runningTimeMicros();
-while(pins.digitalReadPin(Echo)==1 && input.runningTimeMicros()-tim<20000)
-{
-}
-const distance = input.runningTimeMicros() - tim;
-distance = distance / 58;
-return distance;
+export function SR04(Trig: DigitalPin, Echo: DigitalPin): number {
+  pins.digitalWritePin(Trig,1);
+  control.waitMicros(20);
+  pins.digitalWritePin(Trig,0);
+  const tim=input.runningTimeMicros();
+  while(pins.digitalReadPin(Echo)==0 && input.runningTimeMicros()-tim<20000)
+  {
+  }
+  tim=input.runningTimeMicros();
+  while(pins.digitalReadPin(Echo)==1 && input.runningTimeMicros()-tim<20000)
+  {
+  }
+  const distance = input.runningTimeMicros() - tim;
+  distance = distance / 58;
+  return distance;
 }
