b0e728246cc4eb8c251c8262e7f92d7b77dc4c2c
goDrag0n <39076720+goDrag0n@users.noreply.github.com>
2018-05-28 22:12:01 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -65,4 +65,20 @@ namespace mgspace {
     export function ledOfRectangle(length: number, width: number): void {
         basic.showNumber(length * width)
     }
+    //% blockId="SR04" block="SR04 Trig %Trig|Echo %Echo"
+    //% blockGap=2 weight=0 blockExternalInputs=true
+    export function SR04(Trig: DigitalPin, Echo: DigitalPin): number {
+        pins.digitalWritePin(Trig, 1);
+        control.waitMicros(20);
+        pins.digitalWritePin(Trig, 0);
+        const tim = input.runningTimeMicros();
+        while (pins.digitalReadPin(Echo) == 0 && input.runningTimeMicros() - tim < 20000) {
+        }
+        tim = input.runningTimeMicros();
+        while (pins.digitalReadPin(Echo) == 1 && input.runningTimeMicros() - tim < 20000) {
+        }
+        const distance = input.runningTimeMicros() - tim;
+        distance = distance / 58;
+        return distance;
+    }
 }
